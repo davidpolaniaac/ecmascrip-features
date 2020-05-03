5d44e63c07348aa438eeb742c5682f9d7e1c2af4
David Polania <davidpolaniaac@users.noreply.github.com>
2020-05-03 16:51:16 -0500

    add es7

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(9)) {
+  console.log('If the value 7 is found');
+} else {
+  console.log('Is not found')
+}
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
